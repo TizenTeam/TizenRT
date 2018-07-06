01ca955af805ef0fcf3f18eec3f1bb2e38b49cb7
Philippe Coval <p.coval@samsung.com>
2018-07-06 21:58:40 +0200

    WIP: mod: rules/artik/decl.mk (sandbox/rzr/devel/rules/master)
    Change-Id: I7a30014b89286627f56e310ccef469bf71d4a278
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/artik/decl.mk
+++ rules/artik/decl.mk
@@ -37,7 +37,7 @@ platform?=artik
 base_image_type?=minimal
 
 url?=https://github.com/SamsungARTIK/artik-sdk
-machine?=artik055s
+machine?=artik053
 machine_family?=artik05x
 vendor_id?=0403
 product_id?=6010
