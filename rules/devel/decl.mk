b9bcf93b1b9018f78a5e43bf1285ceaf025c5005
Philippe Coval <p.coval@samsung.com>
2018-08-08 13:24:00 +0200

    WIP: mod: rules/devel/decl.mk (sandbox/rzr/devel/iotjs/master)
    Change-Id: I383fc73adefbc496a1c6050fb78d610dc61059a8
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/devel/decl.mk
+++ rules/devel/decl.mk
@@ -42,7 +42,7 @@ defconfig?=${configs_dir}/${machine}/devel/defconfig
 # TODO: Override here if needed:
 platform?=artik
 machine?=${platform}055s
-base_image_type?=nettest
+base_image_type?=iotjs
 
 # Default:
 os?=tinyara
@@ -74,7 +74,7 @@ include rules/iotjs/rules.mk
 #{ devel
 #image_type=iotivity
 #base_image_type=minimal
-base_image_type=nettest
+base_image_type=iotjs
 
 #prep_files+=${private_dir}/config.js
 #prep_files+=external/iotjs/profiles/default.profile
@@ -85,157 +85,3 @@ prep_files+=${contents_dir}
 
 devel_js_minifier?=slimit
 #devel_js_minifier?=yui-compressor
-
-devel/help:
-	@echo "# make demo "
-	@echo "# make demo tty=/dev/ttyUSB2"
-
-devel/todo/iotjs/contents: ${demo_dir} ${contents_dir}
-	@echo "# log: TODO: $<"
-	du -hs ${demo_dir}/ ${contents_dir}/
-	mkdir -p ${contents_dir}/example/
-	rsync -avx ${demo_dir}/ ${contents_dir}/example/
-	rm -rf ${contents_dir}/example/.git*
-	@mkdir -p ${contents_dir}/iotjs/samples
-	rsync -avx external/iotjs/samples/ ${contents_dir}/iotjs/samples/
-	find ${contents_dir} -iname "*~" -exec rm {} \;
-
-iotjs/local:
-	-rm -f external/iotjs
-	rm -rf external/iotjs/
-	rsync -avx  --delete ~/mnt/iotjs/ external/iotjs/
-	${make} iotjs/deps
-
-devel/demo: ${prep_files}
-	${make} -e help configure
-#	grep STARTUP os/.config
-#	grep IOTJS os/.config
-#	grep NETCAT os/.config
-	grep 'BAUD=' os/.config 
-#	${make} -e devel/contents
-	${make} -e deploy
-	${make} -e run 
-#	${make} console/screen
-#	sed -e 's|115200|57600|g' -i os/.config
-
-devel/iotjs/commit: ${demo_dir} external/iotjs/.clang-format
-	ln -fs $^
-	which clang-format-3.9 || sudo apt-get install clang-format-3.9
-	cd $< && clang-format-3.9 -i *.js */*.js
-
-devel/backup: ${CURDIR}/${private_dir}
-	mkdir -p ${HOME}/backup/$</
-	rsync -avx $</ ${HOME}/backup/${<}
-
-
-#external/iotjs/profiles/%:
-#	ls $@ || ${make} iotjs/import
-#	ls $@
-
-app/%: apps/examples/hello
-	@echo "TODO: $@: from $^"
-
-#TODO
-
-local_mk?=rules/local.tmp.mk
-
-devel/start: rules/config.mk clean
-	@echo 'image_type=devel' > ${local_mk}
-#	@echo "TODO"
-#	@echo 'base_image_type=devel' > ${local_mk}
-#	@echo 'image_type?=devel' > ${local_mk}
-#	make base_image_type?=devel
-#	make base_image_type?=devel menuconfig
-
-
-${configs_dir}/${machine}/devel/%:
-	echo 'image_type?=devel' > ${local_mk}
-	git add ${local_mk}
-	git commit -sm "WIP: devel: Update defconfig (${machine})" ${local_mk}
-	${make} defconfig
-
-devel/commit: ${defconfig}
-	ls $<
-	git add $< 
-	git add ${<D}/*.defs
-	git status \
-  && git commit -sm "WIP: devel: Update defconfig (${machine})" ${<D} \
-  || echo "TODO $@"
-	git status \
-  && git commit -sm "WIP: devel Update machine (${machine})" "${configs_dir}/${machine}" \
-  || echo "TODO $@"
-
-devel/diff: ${defconfig} ${config}
-	diff -u $^
-
-devel/del:
-	git status
-	rm -rf ${configs_dir}/${machine}/devel
-	ls ${configs_dir}/${machine}/
-	git commit -sm "WIP: devel: Del (${machine})" "${configs_dir}/${machine}/"
-	echo "TODO: check ${local_mk}"
-
-devel/save: os/.config
-	cp -av $< ${defconfig}
-
-devel/update: devel/save ${defconfig}
-	cp -av ${defconfig} ${base_defconfig}
-
-devel/test: devel/start
-	${make} devel/commit
-	${make} menuconfig run devel/save devel/commit
-#	${make} devel/commit
-	sync
-
-
-#external/iotjs/Kconfig.runtime: external/iotjs/deps/jerry/targets/tizenrt-artik053/apps/jerryscript/Kconfig
-#	ln -fs $< $@
-
--include rules/kconfig-frontends/rules.mk
-#-include rules/iotivity-constrained/rules.mk
-
-${contents_dir}:
-	mkdir -p $@
-
-devel/contents/compress:
-	find ${contents_dir} -iname "*.js" \
-  | while read file ; do \
-  echo "#log: $${file}"; \
-  ${devel_js_minifier} $${file} > $${file}.tmp && mv -v $${file}.tmp $${file} ; \
-  done
-
-devel/contents/del:
-	rm -rf ${contents_dir}
-
-devel/contents: devel/contents/del ${contents_dir} ${contents_rules}
-	@echo "#$@: $^"
-	ls ${contents_dir}
-
-#devel/demo: 
-
-#	@echo "Ready"
-## demo
-
-#TODO
-devel/contents/example: ${contents_dir}/webthing-node
-	rm -rf ${contents_dir}/example
-	rsync -avx ${contents_dir}/webthing-node/ ${contents_dir}/example/
-	cp -av $</example/artik05x-thing.js ${contents_dir}/example/index.js
-
-devel/machine/%:
-	${MAKE} -e machine=${@F} help
-	${MAKE} -e machine=${@F} distclean
-	${MAKE} -e machine=${@F} menuconfig all
-	${MAKE} -e machine=${@F} devel/save devel/update devel/commit
-
-devel/machines: ${defconfigs}
-	ls $^
-	for path in $^ ; do \
- machine=$$(echo "$${path}" \
- | sed -e "s|build/configs/\(.*\)/${base_image_type}/defconfig|\1|g") ;\
- ${MAKE} devel/machine/$${machine} ; \
-done
-
-.PHONY: devel/commit
-
-#} devel
