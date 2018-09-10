62625f1e5e7c256724aa8c05a20d9e040567015b
Philippe Coval <p.coval@samsung.com>
2018-09-10 18:03:53 +0200

    WIP: mod: rules/decl.mk (sandbox/rzr/devel/build/master)
    Change-Id: I666a3c118c05c8993b316ddc4f3782b1f336d397
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/decl.mk
+++ rules/decl.mk
@@ -51,8 +51,8 @@ defconfig?=${configs_dir}/${config_type}/defconfig
 defconfigs?=$(wildcard build/configs/*/${base_image_type}/defconfig)
 configure?=${os_dir}/tools/configure.sh
 config?=${os_dir}/.config
-image?=${build_dir}/output/bin/${project}
-deploy_image?=${image}
+image_file?=${build_dir}/output/bin/${project}
+deploy_image?=${image_file}
 all+=${deploy_image}
 
 prep_files+=${os_dir}/Make.defs
